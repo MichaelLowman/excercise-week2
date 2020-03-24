ba2a3a5f324f19d5e82fbff16d1e1e45309a7a06
MichaelLowman <lowmanm679@gmail.com>
2020-03-24 20:25:02 +0700

    update exercise

---
+++ selasa/sandbox.js
@@ -0,0 +1,23 @@
+var name =' ';
+var peran = ' ';
+
+ if (name == 'jhon' && peran ==''){
+ console.log(`hallo ${name}, pilih peranmu untuk memulai game`);
+
+} else if(name == 'Nina' && peran =='ksatria') {
+ console.log(`Selamat datang di Dunia Proxytia,${name}`);
+
+ console.log(`hallo ${peran} ${name}, kamu dapat menyerang dengan senjatamu`);
+ 
+} else if(name == 'Michael' && peran == 'tabib'){
+    console.log(`"Selamat datang di Dunia Proxytia, ${name}"
+ "Halo ${peran} ${name}, kamu akan membantu temanmu yang berlaku"`);
+
+} else if(name == 'Budi' && peran == 'Penyihir'){
+    console.log(`"Selamat datang di Dunia Proxytia, ${name}"
+    "Halo ${peran} ${name}, ciptakan keajaiban yang membantu kemenanganmu"`)
+} else {
+    console.log(`"Nama harus di isi!"`);
+       
+    
+    }
